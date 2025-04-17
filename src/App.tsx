848bcc174c1f63d605350875e451363f2007b517
Pronothurah <onsongojosphat28@gmail.com>
2025-04-17 16:16:58 +0100

    Fix: github rendering issue

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Index from "./pages/Index";
 import FAQPage from "./pages/FAQPage";
@@ -18,77 +18,75 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Toaster />
         <Sonner />
         <Routes>
           <Route
-            path="/"
-            element={
-              <Layout>
-                <Index />
-              </Layout>
-            }
+        path="/"
+        element={
+          <Layout>
+            <Index />
+          </Layout>
+        }
           />
           <Route
-            path="/solutions"
-            element={
-              <Layout>
-                <Solutions />
-              </Layout>
-            }
+        path="/solutions"
+        element={
+          <Layout>
+            <Solutions />
+          </Layout>
+        }
           />
           <Route
-            path="/products"
-            element={
-              <Layout>
-                <Products />
-              </Layout>
-            }
+        path="/products"
+        element={
+          <Layout>
+            <Products />
+          </Layout>
+        }
           />
           <Route
-            path="/pricing"
-            element={
-              <Layout>
-                <Pricing />
-              </Layout>
-            }
+        path="/pricing"
+        element={
+          <Layout>
+            <Pricing />
+          </Layout>
+        }
           />
           <Route
-            path="/faq"
-            element={
-              <Layout>
-                <FAQPage />
-              </Layout>
-            }
+        path="/faq"
+        element={
+          <Layout>
+            <FAQPage />
+          </Layout>
+        }
           />
           <Route
-            path="/privacy-policy"
-            element={
-              <Layout>
-                <PrivacyPolicyPage />
-              </Layout>
-            }
+        path="/privacy-policy"
+        element={
+            <PrivacyPolicyPage />
+        }
           />
           <Route
-            path="/terms-of-service"
-            element={
-              <Layout>
-                <TermsOfServicePage />
-              </Layout>
-            }
+        path="/terms-of-service"
+        element={
+          <Layout>
+            <TermsOfServicePage />
+          </Layout>
+        }
           />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route
-            path="*"
-            element={
-              <Layout>
-                <NotFound />
-              </Layout>
-            }
+        path="*"
+        element={
+          <Layout>
+            <NotFound />
+          </Layout>
+        }
           />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
